ca1c4ce31f3f3d9d8f4839f39a1089d2ae759125
mrcruz117 <mrcruz1989@gmail.com>
2022-01-19 10:56:55 -0500

    creating csv data with pandas

--- main.py
+++ main.py
@@ -19,11 +19,24 @@ import pandas
 # monday_temp_f = monday_temp * 9 / 5 + 32
 # print(monday_temp_f)
 
-data_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores": [76, 56, 65]
+# data_dict = {
+#     "students": ["Amy", "James", "Angela"],
+#     "scores": [76, 56, 65]
+# }
+#
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("student_scores.csv")
+# print(data)
+data = pandas.read_csv("2018_Squirrel_Data.csv")
+gray_squirrels = data[data["Primary Fur Color"] == "Gray"]
+black_squirrels = data[data["Primary Fur Color"] == "Black"]
+cinnamon_squirrels = data[data["Primary Fur Color"] == "Cinnamon"]
+fur_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [len(gray_squirrels), len(black_squirrels), len(cinnamon_squirrels)]
 }
 
-data = pandas.DataFrame(data_dict)
-data.to_csv("student_scores.csv")
-print(data)
+df = pandas.DataFrame(fur_dict)
+df.to_csv("squirrel_count.csv")
+
+print((fur_dict))
