a6c5387a7709a1df52bba846b532e0d8f5631a96
mrcruz117 <mrcruz1989@gmail.com>
2022-01-19 09:42:40 -0500

    installed and used pandas to visualize data

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-import csv
+# import csv
+#
+# with open("weather_data.csv") as weather_data:
+#     data = csv.reader(weather_data)
+#
+#     temperatures = []
+#     for row in data:
+#         if row[1] != 'temp':
+#             temperatures.append(int(row[1]))
+#
+#     print(temperatures)
 
-with open("weather_data.csv") as weather_data:
-    data = csv.reader(weather_data)
+import pandas
 
-    temperatures = []
-    for row in data:
-        if row[1] != 'temp':
-            temperatures.append(int(row[1]))
+data = pandas.read_csv("weather_data.csv")
 
-    print(temperatures)
+print(data["temp"])
